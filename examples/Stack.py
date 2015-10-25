f378c81d6cd75b74470b4ddd8273c061b6001cff
harisankar.krishna <harisankar.krishna@outlook.com>
2015-10-25 11:52:30 +0530

    stack examples working

---
+++ examples/Stack.py
@@ -0,0 +1,27 @@
+'''
+Created on Oct 25, 2015
+@author: hari
+'''
+from datastructures.stackNqueues.Stack import Stack
+
+if __name__ == '__main__':
+    stack = Stack()
+    # count items
+    print 'Stack items count: %s' % str(stack.size)
+    #Push items
+    stack.push(element = 1)
+    stack.push(element = 2)
+    stack.push(element = 3)
+    stack.push(element = 4)
+    stack.push(element = 5)
+    stack.push(element = 6)
+    #top
+    print 'Stack top is %s' % str(stack.top)
+    #pop
+    popped = stack.pop()
+    print 'Popped item is %s' % str(popped)
+    #iterate over the stack popping items one by one
+    print 'Iterate over stack popping items'
+    for item in stack:
+        print 'item %s' % str(item)
+        

bbb340403663302c84542e824932ee499fd1a489
harisankar.krishna <harisankar.krishna@outlook.com>
2015-10-28 23:34:46 +0530

    Disjoint sets with union using uptree nodes.

--- coils/sets/disjoint_sets_with_union.py
+++ coils/sets/disjoint_sets_with_union.py
@@ -2,7 +2,11 @@
 Created on Oct 28, 2015
 @author: hari
 '''
-class DisjointSetForest(object):
+from coils.hashtables.hashbuckets import SplayedHashBucket
+from coils.hashtables.HashTables import SeperateChainHashTable
+from coils.trees.uptree import UpTreeNode
+
+class DisjointSetWithUnion(object):
     '''
     A disjoint set forest has disjoint sets where each set is represented by an uptree.
     
@@ -16,8 +20,68 @@ class DisjointSetForest(object):
     for more info please refer Datastructures and their algorithms by Harry Lewis and Larry Denenberg
     
     uptree nodes are used to represent the individual items of the set.
-    1) Each item needs to be known just like that in the set.
-    2) 
+    1) Each item needs to be known just like that in the set. We use a hash tabe where table[item] = UpTree Node for the item
+    2) make_set(item) just creates an uptree node with item. Each node starts off as a tree rooted at node.   
     '''
+    def __init__(self):
+        self._table_of_uptrees = SeperateChainHashTable(bucket_type_class = SplayedHashBucket)
     
+    def make_set(self, item = None):
+        if item == None:
+            return None
+        uptree_node = UpTreeNode(node_element = item)#single node tree
+        self._table_of_uptrees[item] = uptree_node
+        return uptree_node
     
+    def _uptree_find_with_path_compression(self, item = None):
+        '''
+        Return the root of the uptree to which item belongs. Compress the path before leaving.
+        '''
+        uptree_node_with_item = self._table_of_uptrees.get(item, None)
+        if uptree_node_with_item == None:
+            raise KeyError('Key Error: %s ' % repr(item))
+        #before returning the root node element, we set the parent of the original node to the parent.
+        current_node = uptree_node_with_item
+        while current_node.parent_node != None:
+            current_node = current_node.parent_node
+        #we have root node
+        root_of_uptree = current_node
+        #compress path
+        current_node = uptree_node_with_item
+        parent_of_current_node = current_node.parent_node 
+        while current_node != root_of_uptree:
+            parent_of_current_node = current_node.parent_node
+            current_node.parent_node = root_of_uptree
+            current_node = parent_of_current_node
+            
+        return root_of_uptree
+        
+    def find(self, item):
+        '''
+        returns the set to which this item belongs.
+        '''
+        try:
+            root_node = self._uptree_find_with_path_compression(item)
+        except KeyError:
+            return  None
+        return root_node.node_element
+        
+    def union(self, item_1 = None, item_2 = None):
+        '''
+        Finds the set for item_1 and item_2. Merges the two and returns the larger set.
+        '''
+        if item_1 == None or item_2 == None:
+            return None
+        
+        uptree_1 = self._uptree_find_with_path_compression(item = item_1)#root actually
+        uptree_2 = self._uptree_find_with_path_compression(item = item_2)#root actually
+        
+        #return the remaining dominating tree as first element of the tuple
+        if uptree_1.node_count >= uptree_2.node_count:
+            uptree_2.parent_node = uptree_1
+            uptree_1.node_count = uptree_1.node_count + uptree_2.node_count
+            return uptree_1.node_element
+        else:
+            uptree_1.parent_node = uptree_2
+            uptree_2.node_count = uptree_2.node_count + uptree_1.node_count
+            return uptree_2

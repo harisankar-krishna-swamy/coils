cbc878880d7ff882ee3b19a8ba096a1939c710bf
Hari <harisankar.krishna@outlook.com>
2018-07-15 20:46:01 +0530

    added setup.py and license

---
+++ src/pycoils/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pycoils",
+    version="1.6.0",
+    author="Harisankar Krishna Swamy",
+    author_email="harisankar.krishna@outlook.com",
+    description="A Python datastructure library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/harisankar-krishna-swamy-code/coils",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+)
